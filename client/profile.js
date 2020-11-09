ff9a39fb31d33bea65afe5334511f07ddad72b80
Justin Diep <justintd1034@gmail.com>
2020-11-09 09:02:16 -0500

    added fetch for user parts table

--- client/profile.js
+++ client/profile.js
@@ -2,7 +2,32 @@ window.addEventListener("load", async function() {
     const userPartsRequest = await fetch("./userParts");
     const userPartsData = userPartsRequest.ok ? await userPartsRequest.json() : [];
 
+    let rowNum = 1;
     for (const userPart of userPartsData) {
         const tr = document.createElement('tr');
+        const th = document.createElement('th');
+        const id = document.createElement('td');
+        const name = document.createElement('td');
+        const type = document.createElement('td');
+        const cost = document.createElement('td');
+        const link = document.createElement('td');
+        
+        $scope.th = "row";
+        th.innerText = rowNum.toString();
+        rowNum += 1;
+
+        id.innerText = userPart.id;
+        name.innerText = userPart.name;
+        type.innerText = userPart.type;
+        cost.innerText = userPart.cost;
+        link.innerText = userPart.link;
+
+        tr.appendChild(th);
+        tr.appendChild(id);
+        tr.appendChild(name);
+        tr.appendChild(type);
+        tr.appendChild(cost);
+        tr.appendChild(link);
     }
+
 })
