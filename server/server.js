9a2e1dc1be2a98d6f06aaff3c38b91a9d1138c2a
Andrew Leger <andrewleger9709@gmail.com>
2020-11-10 01:47:41 -0500

    added price faker

--- server/server.js
+++ server/server.js
@@ -36,37 +36,6 @@ app.get('/', (req, res) => {
     }
     // res.send(readFileSync(path));
 });
-// app.get('/profile', (req, res) => {
-//     const path = 'client/profilePage.html';
-//     console.log('Trying to serve: profilePage');
-//     if (existsSync(path)) {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(readFileSync(path));
-//         res.end();
-//     }
-//     // res.send(readFileSync(path));
-// });
-
-// app.get('/social', (req, res) => {
-//     const path = 'client/SocialPage.html';
-//     console.log('Trying to serve: SocialPage');
-//     if (existsSync(path)) {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(readFileSync(path));
-//         res.end();
-//     }
-//     // res.send(readFileSync(path));
-// });
-// app.get('/info', (req, res) => {
-//     const path = 'client/InfoPage.html';
-//     console.log('Trying to serve: InfoPage');
-//     if (existsSync(path)) {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(readFileSync(path));
-//         res.end();
-//     }
-//     // res.send(readFileSync(path));
-// });
 app.get('/switches', (req, res) => {
     // Should we be sending dummy data or something? I'm honestly not sure.
     res.send('Switch data I guess?');
@@ -104,7 +73,8 @@ function blob() {
     const name = faker.commerce.productName();
     const id = faker.random.number();
     const desc = faker.commerce.productDescription();
-    return { imgSource: imgSource, imgDesc: imgDesc, name: name, id: id, desc: desc };
+    const price = faker.commerce.price();
+    return { imgSource: imgSource, imgDesc: imgDesc, name: name, id: id, desc: desc, price: price };
 }
 function writeBlob(res) {
     const array = [];
