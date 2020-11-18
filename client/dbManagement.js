044716ed9bef795a3b273e4e0f07fe6c716feb92
Andrew Leger <andrewleger9709@gmail.com>
2020-11-18 17:27:24 -0500

    Database Changes

--- client/dbManagement.js
+++ client/dbManagement.js
@@ -9,8 +9,8 @@ const pgp = require('pg-promise')({
 });
 
 const username = 'postgres';
-const password = 'admin';
-const psqlURL = process.env.DATABASE_URL || `postgres://${username}:${password}@localhost/`;
+const password = 'postgres';
+const psqlURL = process.env.DATABASE_URL || `postgres://${username}:${password}@localhost/psiproject`;
 const db = pgp(psqlURL);
 
 console.log('Trying to find:', psqlURL);
@@ -32,6 +32,7 @@ async function connectAndRun(task) {
     }
 }
 
+// The next 5 functions are generic select * statements for each table in the database. TODO: Add tables for login and profile
 async function getPCBs() {
     return await connectAndRun(db => db.any('SELECT * FROM PCBs'));
 }
@@ -48,11 +49,45 @@ async function getCables() {
     return await connectAndRun(db => db.any('SELECT * FROM Cables'));
 }
 
+
+
+// Retrieve a table of cases compatible with a chosen pcb
+// Return value: List of objects, each containing a tuple of the table
+async function getCasesFromPCBs(pcb_size) {
+    return await connectAndRun(db => db.any('SELECT c.itemId, c.partname, c.partDescription, c.pcb_size FROM pcbs p join cases c on p.PCB_size=c.PCB_size WHERE p.PCB_size=$1;', [pcb_size]));
+}
+
+// Retrieve a tables of switches compatible with a chosen pcb
+// Return value: List of objects, each containing a tuple of the table
+async function getSwitchesFromPCBs(switch_type) {
+    return await connectAndRun(db => db.any('SELECT s.itemID, s.partname, s.partdescription, s.switch_type FROM pcbs p join switches s on p.switch_type=s.switch_type WHERE p.switch_type=$1;', [switch_type]));
+}
+
+
+
+
+
+
+
+
+
+
+// testing here
 // (async () => {
-//     const r = await getCables();
+//     const r = await getCasesFromPCBs('80');
 //     console.log(r);
 // })();
 
-console.log('hi');
+// console.log('hi');
 
-module.exports.getCables = getCables;
+// Export functions
+module.exports = {
+    getPCBs: getPCBs,
+    getCases: getCases,
+    getSwitches: getSwitches,
+    getkeyCaps: getkeyCaps,
+    getCables: getCables,
+    getCasesFromPCBs: getCasesFromPCBs,
+    getSwitchesFromPCBs: getSwitchesFromPCBs
+
+};
