358a9e329f187442164dcf7877efd9c5abfd6b0c
Andrew Leger <andrewleger9709@gmail.com>
2020-11-22 01:36:58 -0500

    removing junk

--- deprecated/social.js
+++ deprecated/social.js
@@ -1,32 +1,33 @@
-window.addEventListener("load", async function() {
-    const apiResult = await fetch("./socialGet");
-    const apiData = apiResult.ok ? await apiResult.json() : [];
+window.addEventListener("load", async function () {
+  const apiResult = await fetch("./socialGet");
+  const apiData = apiResult.ok ? await apiResult.json() : [];
 
-    const container = document.getElementById('socialBody');
-/* const apiData = [{
-  cardTitle: "cardTitle1",
-  date:'Oct 12, 1999'
-  email: "myEmail@hotmail.com",
-  bodyText: "out1"
-}, {
-  cardTitle: "cardTitle2",
-  date:'Oct 22, 1999'
-  email: "notMyEmail@hotmail.com",
-  bodyText: "out2"
-}, {
-  cardTitle: "cardTitle1",
-  date:'Oct 32, 1999'
-  email: "myRealEmail@hotmail.com",
-  bodyText: "out3"
-}]; */
+  const container = document.getElementById('socialBody');
+  /* const apiData = [{
+    cardTitle: "cardTitle1",
+    date:'Oct 12, 1999'
+    email: "myEmail@hotmail.com",
+    bodyText: "out1"
+  }, {
+    cardTitle: "cardTitle2",
+    date:'Oct 22, 1999'
+    email: "notMyEmail@hotmail.com",
+    bodyText: "out2"
+  }, {
+    cardTitle: "cardTitle1",
+    date:'Oct 32, 1999'
+    email: "myRealEmail@hotmail.com",
+    bodyText: "out3"
+  }]; */
 
-apiData.forEach((result, idx) => {
-  // Create card element
-  const card = document.createElement('div');
-  card.classList = 'card-body';
+  // eslint-disable-next-line no-unused-vars
+  apiData.forEach((result, idx) => {
+    // Create card element
+    const card = document.createElement('div');
+    card.classList = 'card-body';
 
-  // Construct card content
-  const content = `
+    // Construct card content
+    const content = `
     <div class="card" style="width: 95%;">
             <div class="card-body">
                 <h6 class="card-subtitle mb-2 text-muted">${result.date}</h6>
@@ -38,8 +39,8 @@ apiData.forEach((result, idx) => {
         </div>
   `;
 
-  // Append newyly created card element to the container
-  container.innerHTML += content;
-})
+    // Append newyly created card element to the container
+    container.innerHTML += content;
+  });
 
-})
+});
